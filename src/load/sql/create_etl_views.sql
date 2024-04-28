cc821ffd03a591a05c21fb1dc587cde86d9015b8
David <dr00b@users.noreply.github.com>
2024-04-28 16:52:57 -0400

    Minor change

--- src/load/sql/create_etl_views.sql
+++ src/load/sql/create_etl_views.sql
@@ -1,17 +1,1 @@
--- # Owners
--- CMSDataset('Hospital All Owners', '029c119f-f79c-49be-9100-344d31d10344', '2024-04', 'hospital_all_owners'),
--- CMSDataset('Home Health Agency All Owners', 'fc009b2d-7846-44b1-b4a1-692f0c143879', '2024-04', 'hha_all_owners'),
--- CMSDataset('Hospice All Owners', 'e983965e-1603-4cb8-82b5-c40090e380d1', '2024-04', 'hospice_all_owners'),
--- CMSDataset('Skilled Nursing Facilities All Owners', 'afe44b85-cc6d-40d7-b5df-00ae8910d1d2', '2024-04', 'snf_all_owners'),
--- CMSDataset('Federally Qualified Health Center All Owners', 'ed289c89-0bb8-4221-a20a-85776066381b', '2024-04', 'fqhc_all_owners'),
--- CMSDataset('Rural Health Clinic All Owners', 'ab03c9bc-0c22-4ca4-b032-21dd3408210d', '2024-04', 'rhc_all_owners'),
-
--- # Enrollments
--- CMSDataset('Hospital Enrollments', 'f6f6505c-e8b0-4d57-b258-e2b94133aaf2', '2024-04', 'hospital_enrollments'),
--- CMSDataset('Home Health Agency Enrollments', '15f64ab4-3172-4a27-b589-ebd67a6d28aa', '2024-04', 'hha_enrollments'),
--- CMSDataset('Hospice Enrollments', '25704213-e833-4b8b-9dbc-58dd17149209', '2024-04', 'hospice_enrollments'),
--- CMSDataset('Skilled Nursing Facility Enrollments', '5f2c306f-3b1c-42cd-b037-187b2ce22126', '2024-04', 'snf_enrollments'),
--- CMSDataset('Federally Qualified Health Center Enrollments', '4bcae866-3411-439a-b762-90a6187c194b', '2024-04', 'fqhc_enrollments'),
--- CMSDataset('Rural Health Clinic Enrollments', '3b7e7659-067e-41ea-8e36-f9ee2036e1f6', '2024-04', 'rhc_enrollments')
-
-
+-- TODO, compile list of individuals / orgs across all entities.
